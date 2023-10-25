fdb1876b4f636a4abfb6c57cb321d78989ff0704
xcheche <chekwubechukwu23@gmail.com>
2023-10-25 17:45:51 +0100

    django

---
+++ portfolio/employee/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def employee(request):
+    return HttpResponse("This is the employee page")
+
+def profile(request):
+    return HttpResponse("This is the profile page")
